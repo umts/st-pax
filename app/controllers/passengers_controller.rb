9fa1ed525f4830c8ced13a9047d78be3f37d7771
avacmeyer <ameyer@umass.edu>
2017-05-31 17:10:06 -0400

    passenger controller bones

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,14 +1,42 @@
 class PassengersController < ApplicationController
+  def new
+    @passenger = Passenger.new
+  end
+
   def index
+    @passengers = Passenger.all
   end
+
   def show
+    @passenger = Passenger.find(params[:id])
   end
+
   def edit
+    @passenger = Passenger.find(params[:id])
   end
+
   def create
+    passenger = Passenger.new(params[:passenger])
+    if passenger.save
+      flash[:notice] = "Passenger has been created."
+    else 
+      flash[:error] = passenger.errors.full_messages.to_sentence
+    end
   end
+  
   def update
+    passenger = Passenger.find(params[:id])
+    if passengers.update params[:passenger] 
+      flash[:notice] = "Passenger has been updated"
+    else
+      flash[:notice] = passenger.errors.full_messages.to_sentence
+    end
   end
+  
   def destroy
+    passenger = Passenger.find(params[:id])
+    passenger.destroy
+    flash[:notice] = "Passenger has been deleted"
+    redirect_to action: 'index'
   end
 end
