2a23322b2d4060878039d8fa8460e834c423ffe9
avacmeyer <ameyer@umass.edu>
2017-06-09 12:43:12 -0400

    login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users
   root 'passengers#index'
   resources :passengers
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
