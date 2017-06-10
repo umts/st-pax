8c3c6fcd412966213626ac99e557c5f6528ece9e
anbranin <keichelman@gmail.com>
2017-06-10 16:29:57 -0400

    add localization options

---
+++ app/assets/javascripts/localization.js
@@ -0,0 +1,19 @@
+/*
+ $('.my-datepicker-here').datepicker({
+    language: 'en'
+});
+*/
+//TODO: Find out why the above does not manually initialize datepickers
+
+$.fn.datepicker.language['en'] = {
+    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    months: ['January','February','March','April','May','June', 'July','August','September','October','November','December'],
+    monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    today: 'Today',
+    clear: 'Clear',
+    dateFormat: 'mm/dd/yyyy',
+    timeFormat: 'hh:ii aa',
+    firstDay: 0
+};
