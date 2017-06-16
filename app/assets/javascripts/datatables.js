fd594c148dbd989e589726f6f4e137f237d47469
Chauce <ccchurch@umass.edu>
2017-06-16 17:49:02 -0400

    Added DataTables jQuery plug-in

---
+++ app/assets/javascripts/datatables.js
@@ -0,0 +1,13 @@
+$(document).ready( function () {$('#passengers').DataTable({
+  "aoColumns": [
+    null,
+    null,
+    null,
+    { "bSortable": false },
+    { "bSortable": false },
+    null,
+    { "bSortable": false },
+    { "bSortable": false },
+    { "bSortable": false },
+  ]
+});});
