957104f687c7aadc44141ae19da8ca438b92e9b4
Chauce <ccchurch@umass.edu>
2017-06-23 16:55:56 -0400

    create users in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,43 +10,33 @@
 require 'ffaker'
 expirations = ((Date.today - 7.days)..(Date.today + 6.months)).to_a
 
-10.times do
-  Passenger.create! name: FFaker::Name.name,
-                    address: FFaker::Address.street_address,
-                    email: FFaker::Internet.email,
-                    phone: FFaker::PhoneNumber.short_phone_number,
-                    wheelchair: false,
-                    active: true,
-                    permanent: false,
-                    expiration: expirations.sample
-  Passenger.create! name: FFaker::Name.name,
-                    address: FFaker::Address.street_address,
-                    email: FFaker::Internet.email,
-                    phone: FFaker::PhoneNumber.short_phone_number,
-                    wheelchair: false,
-                    active: true,
-                    permanent: true
+20.times do
+  [nil, Date.today + 2.days, Date.today - 2.days, Date.today - 4.days, expirations.sample].each do |exp|
+    Passenger.create! name: FFaker::Name.name,
+                      address: FFaker::Address.street_address,
+                      email: FFaker::Internet.email,
+                      phone: FFaker::PhoneNumber.short_phone_number,
+                      wheelchair: [true, false].sample,
+                      active: true,
+                      permanent: [true, false].sample,
+                      expiration: exp
+  end
 end
 
-Passenger.create! name: FFaker::Name.name,
-                  address: FFaker::Address.street_address,
-                  email: FFaker::Internet.email,
-                  phone: FFaker::PhoneNumber.short_phone_number,
-                  wheelchair: true,
-                  active: true,
-                  permanent: false
-Passenger.create! name: FFaker::Name.name,
-                  address: FFaker::Address.street_address,
-                  email: FFaker::Internet.email,
-                  phone: FFaker::PhoneNumber.short_phone_number,
-                  wheelchair: false,
-                  active: true,
-                  permanent: false
-Passenger.create! name: FFaker::Name.name,
-                  address: FFaker::Address.street_address,
-                  email: FFaker::Internet.email,
-                  phone: FFaker::PhoneNumber.short_phone_number,
-                  wheelchair: false,
-                  active: true,
-                  permanent: false,
-                  expiration: '2017-06-13'
+5.times do
+  User.create! name: FFaker::Name.name,
+                email: FFaker::Internet.email,
+                phone: FFaker::PhoneNumber.short_phone_number,
+                spire: rand(10 ** 8).to_s.rjust(8,'0'),
+                active: true,
+                admin: false
+end
+
+2.times do
+  User.create! name: FFaker::Name.name,
+                email: FFaker::Internet.email,
+                phone: FFaker::PhoneNumber.short_phone_number,
+                spire: rand(10 ** 8).to_s.rjust(8,'0'),
+                active: true,
+                admin: true
+end
