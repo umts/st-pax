01f9bf9a965c6a00f0001d6742fff2b2b9dce101
Chauce <ccchurch@umass.edu>
2017-06-26 15:11:49 -0400

    indents now correct

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,18 +25,18 @@ end
 
 5.times do
   User.create! name: FFaker::Name.name,
-                email: FFaker::Internet.email,
-                phone: FFaker::PhoneNumber.short_phone_number,
-                spire: rand(10 ** 8).to_s.rjust(8,'0'),
-                active: true,
-                admin: false
+               email: FFaker::Internet.email,
+               phone: FFaker::PhoneNumber.short_phone_number,
+               spire: rand(10 ** 8).to_s.rjust(8,'0'),
+               active: true,
+               admin: false
 end
 
 2.times do
   User.create! name: FFaker::Name.name,
-                email: FFaker::Internet.email,
-                phone: FFaker::PhoneNumber.short_phone_number,
-                spire: rand(10 ** 8).to_s.rjust(8,'0'),
-                active: true,
-                admin: true
+               email: FFaker::Internet.email,
+               phone: FFaker::PhoneNumber.short_phone_number,
+               spire: rand(10 ** 8).to_s.rjust(8,'0'),
+               active: true,
+               admin: true
 end
