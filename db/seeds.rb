7dc2199eae6a9c7d0f6e2d206c7b3bc1be0fae71
Chauce <ccchurch@umass.edu>
2017-06-30 21:36:44 -0400

    Seeds for PAX and Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,21 @@ expirations = ((Date.today - 7.days)..(Date.today + 6.months)).to_a
                       expiration: exp
   end
 end
+
+5.times do
+  User.create! name: FFaker::Name.name,
+               email: FFaker::Internet.email,
+               phone: FFaker::PhoneNumber.short_phone_number,
+               spire: rand(10 ** 8).to_s.rjust(8,'0'),
+               active: true,
+               admin: false
+end
+
+2.times do
+  User.create! name: FFaker::Name.name,
+               email: FFaker::Internet.email,
+               phone: FFaker::PhoneNumber.short_phone_number,
+               spire: rand(10 ** 8).to_s.rjust(8,'0'),
+               active: true,
+               admin: true
+end
