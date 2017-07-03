d5577204e3d842096cf657bf80d9c4f9cb0192ca
Chauce <ccchurch@umass.edu>
2017-07-03 14:00:42 -0400

    Testing w/ more PAX in seeds, added unauth message

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'ffaker'
 expirations = ((Date.today - 7.days)..(Date.today + 6.months)).to_a
 
-20.times do
+50.times do
   [nil, Date.today + 2.days, Date.today - 2.days, Date.today - 4.days, expirations.sample].each do |exp|
     Passenger.create! name: FFaker::Name.name,
                       address: FFaker::Address.street_address,
