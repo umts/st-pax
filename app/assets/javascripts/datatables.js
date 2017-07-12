59b3cc0125813f8eedf2dc44ace23d0672429dd5
Chauce <ccchurch@umass.edu>
2017-07-12 14:30:39 -0400

    Indentation is fixed maybe?

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -1,32 +1,32 @@
 $( document ).on("turbolinks:load", function() {
   $('#passengers.admin-table').DataTable({
-  "aoColumns": [
-    null,
-    null,
-    null,
-    { "bSortable": false },
-    { "bSortable": false },
-    null,
-    { "bSortable": false },
-    { "bSortable": false },
-    { "bSortable": false },
-  ],
-  "aLengthMenu": [[25, 50, 75, 100, -1], [25, 50, 75, 100, "All"]],
-  "iDisplayLength": 100
-});
+    "aoColumns": [
+      null,
+      null,
+      null,
+      { "bSortable": false },
+      { "bSortable": false },
+      null,
+      { "bSortable": false },
+      { "bSortable": false },
+      { "bSortable": false },
+    ],
+    "aLengthMenu": [[25, 50, 75, 100, -1], [25, 50, 75, 100, "All"]],
+    "iDisplayLength": 100
+  });
 
   $('#passengers.dispatch-table').DataTable({
-  "aoColumns": [
-    null,
-    null,
-    null,
-    { "bSortable": false },
-    { "bSortable": false },
-    null,
-    { "bSortable": false },
-    { "bSortable": false },
-  ],
-  "aLengthMenu": [[25, 50, 75, 100, -1], [25, 50, 75, 100, "All"]],
-  "iDisplayLength": 100
-});
+    "aoColumns": [
+      null,
+      null,
+      null,
+      { "bSortable": false },
+      { "bSortable": false },
+      null,
+      { "bSortable": false },
+      { "bSortable": false },
+    ],
+    "aLengthMenu": [[25, 50, 75, 100, -1], [25, 50, 75, 100, "All"]],
+    "iDisplayLength": 100
+  });
 });
