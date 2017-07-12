387a21c50eb9fc0eb0430822263423cb8db91e7c
Chauce <ccchurch@umass.edu>
2017-07-12 14:34:35 -0400

    Indentation is fixed maybe?

--- app/assets/javascripts/input_disable.js
+++ app/assets/javascripts/input_disable.js
@@ -1,10 +1,10 @@
 $(document).on('turbolinks:load', function(){
   $('input#passenger_permanent').change(function(){
-      var expirationField = $('input#passenger_expiration')
-      if($(this).is(':checked')){
-        expirationField.prop('disabled', true);
-        expirationField.val('');
-      }
-      else expirationField.prop('disabled', false);
+    var expirationField = $('input#passenger_expiration')
+    if($(this).is(':checked')){
+      expirationField.prop('disabled', true);
+      expirationField.val('');
+    }
+    else expirationField.prop('disabled', false);
   });
 });
