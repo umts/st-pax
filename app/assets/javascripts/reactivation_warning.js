96906913760d670e791194144d54d415597b34b0
Chauce <ccchurch@umass.edu>
2017-07-13 11:34:14 -0400

    fixed indentation

--- app/assets/javascripts/reactivation_warning.js
+++ app/assets/javascripts/reactivation_warning.js
@@ -1,9 +1,9 @@
 $(document).on('turbolinks:load', function(){
   $('input#passenger_active').change(function(){
-      if($(this).is(':checked')){
-        alert("You are about to temporarily reactivate this passenger. \
-              \n Please note that, unless an updated doctor's note is \
-              \n obtained, this passenger will deactivate again at 4:30am.");
-      }
+    if($(this).is(':checked')){
+      alert("You are about to temporarily reactivate this passenger. \
+      \n Please note that, unless an updated doctor's note is \
+      \n obtained, this passenger will deactivate again at 4:30am.");
+    }
   });
 });
