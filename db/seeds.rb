2858471d3487949dbfa03792baf60e3acc6e30cb
avacmeyer <ameyer@umass.edu>
2017-07-25 14:11:30 -0400

    rubocop fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@ require 'ffaker'
 expirations = ((Date.today - 7.days)..(Date.today + 6.months)).to_a
 
 50.times do
-  [nil, Date.today + 2.days, Date.today - 2.days, Date.today - 4.days, expirations.sample].each do |exp|
+  [nil, Date.today + 2.days, Date.today - 2.days, Date.today - 4.days,
+   expirations.sample].each do |exp|
     Passenger.create! name: FFaker::Name.name,
                       address: FFaker::Address.street_address,
                       email: FFaker::Internet.email,
@@ -18,7 +19,7 @@ end
   User.create! name: FFaker::Name.name,
                email: FFaker::Internet.email,
                phone: FFaker::PhoneNumber.short_phone_number,
-               spire: rand(10 ** 8).to_s.rjust(8, '0'),
+               spire: rand(10**8).to_s.rjust(8, '0'),
                active: true,
                admin: false
 end
@@ -27,7 +28,7 @@ end
   User.create! name: FFaker::Name.name,
                email: FFaker::Internet.email,
                phone: FFaker::PhoneNumber.short_phone_number,
-               spire: rand(10 ** 8).to_s.rjust(8, '0'),
+               spire: rand(10**8).to_s.rjust(8, '0'),
                active: true,
                admin: true
 end
