2ce4d0b1fd812b6b3d2f25c94d054a766109518e
Chauce <ccchurch@umass.edu>
2017-08-08 12:40:05 -0400

    removed PAX seeds for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,4 @@
 require 'ffaker'
-expirations = ((Date.today - 7.days)..(Date.today + 6.months)).to_a
-
-50.times do
-  [nil, Date.today + 2.days, Date.today - 2.days, Date.today - 4.days,
-   expirations.sample].each do |exp|
-    Passenger.create! name: FFaker::Name.name,
-                      address: FFaker::Address.street_address,
-                      email: FFaker::Internet.email,
-                      phone: FFaker::PhoneNumber.short_phone_number,
-                      wheelchair: [true, false].sample,
-                      active: true,
-                      permanent: [true, false].sample,
-                      expiration: exp
-  end
-end
 
 5.times do
   User.create! name: FFaker::Name.name,
