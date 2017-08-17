ab805ab9e3ec440592956881bf159feacc867cbd
David Faulkenberry <david.faulkenberry@gmail.com>
2017-08-17 17:07:24 -0400

    Highlight the current link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def navbar_link(text, url)
+    button_class = 'button btn btn-default navbar-btn navbar-left'
+    button_class += ' current' if current_page? url
+    link_to url do
+      content_tag :button, text, class: button_class
+    end
+  end
 end
