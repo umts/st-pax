e61003936891c11c9ae3861f437f2a25060f91b0
AkivaGreen <green.akiva@outlook.com>
2017-08-17 17:07:47 -0400

    Passenger management feature spec

--- spec/features/passenger_management_spec.rb
+++ spec/features/passenger_management_spec.rb
@@ -2,21 +2,28 @@ require 'rails_helper'
 
 feature 'Passenger Management' do 
   before :each do
-    sign_in(create :user, :admin) 
+    sign_in(create :user, :admin)
   end
   scenario 'creating a new passenger' do 
     visit passengers_path
+    click_link 'Add New Passenger'
+    fill_in('passenger[name]', with: 'Foo Bar')
+    fill_in('passenger[email]', with: 'foobar@invalid.com')
+    click_button('Submit')
+    expect(page).to have_text('Passenger was successfully created.')
   end
   scenario 'editing an existing passenger' do 
     passenger = create :passenger
     visit passengers_path
-  end
-  scenario 'viewing an existing passenger' do 
-    passenger = create :passenger
-    visit passengers_path
+    click_link 'Edit'
+    check('passenger_permanent')
+    click_button('Submit')
+    expect(page).to have_text('Passenger was successfully updated.')
   end
   scenario 'deleting an existing passenger' do 
     passenger = create :passenger
     visit passengers_path
+    click_link 'Delete'
+    expect(page).to have_text('Passenger was successfully destroyed.')
   end
 end
