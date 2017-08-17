1a66ad75327799334f367e619e15886ab04546b2
AkivaGreen <green.akiva@outlook.com>
2017-08-17 16:11:58 -0400

    Updated rails and spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,9 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+require 'rspec-html-matchers'
+require 'rspec/rails'
+require 'capybara/rails'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
