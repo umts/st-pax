2727d66d50304cf289f6021769dd863d7f2729c7
AkivaGreen <green.akiva@outlook.com>
2017-08-22 12:04:50 -0400

    Feature spec for dispatcher passenger management

--- spec/features/passenger_management_spec.rb
+++ spec/features/passenger_management_spec.rb
@@ -3,29 +3,57 @@
 require 'rails_helper'
 
 feature 'Passenger Management' do 
-  before :each do
-    sign_in(create :user, :admin)
+  feature 'as and admin' do 
+    before :each do
+      sign_in(create :user, :admin)
+    end
+    scenario 'creating a new passenger' do 
+      visit passengers_path
+      click_link 'Add New Passenger'
+      fill_in('passenger[name]', with: 'Foo Bar')
+      fill_in('passenger[email]', with: 'foobar@invalid.com')
+      click_button('Submit')
+      expect(page).to have_text('Passenger was successfully created.')
+    end
+    scenario 'editing an existing passenger' do 
+      passenger = create :passenger
+      visit passengers_path
+      click_link 'Edit'
+      check('passenger_permanent')
+      click_button('Submit')
+      expect(page).to have_text('Passenger was successfully updated.')
+    end
+    scenario 'deleting an existing passenger' do 
+      passenger = create :passenger
+      visit passengers_path
+      click_link 'Delete'
+      expect(page).to have_text('Passenger was successfully destroyed.')
+    end
   end
-  scenario 'creating a new passenger' do 
-    visit passengers_path
-    click_link 'Add New Passenger'
-    fill_in('passenger[name]', with: 'Foo Bar')
-    fill_in('passenger[email]', with: 'foobar@invalid.com')
-    click_button('Submit')
-    expect(page).to have_text('Passenger was successfully created.')
-  end
-  scenario 'editing an existing passenger' do 
-    passenger = create :passenger
-    visit passengers_path
-    click_link 'Edit'
-    check('passenger_permanent')
-    click_button('Submit')
-    expect(page).to have_text('Passenger was successfully updated.')
-  end
-  scenario 'deleting an existing passenger' do 
-    passenger = create :passenger
-    visit passengers_path
-    click_link 'Delete'
-    expect(page).to have_text('Passenger was successfully destroyed.')
+  feature 'as a dispatcher' do 
+    before :each do 
+      sign_in(create :user)
+    end
+    scenario 'creating a new passenger' do 
+      visit passengers_path
+      click_link 'Add New Passenger'
+      fill_in('passenger[name]', with: 'Foo Bar')
+      fill_in('passenger[email]', with: 'foobar@invalid.com')
+      click_button('Submit')
+      expect(page).to have_text('Passenger was successfully created.')
+    end
+    scenario 'editing an existing passenger' do 
+      passenger = create :passenger, name: 'Foo Bar'
+      visit passengers_path
+      click_link 'Edit'
+      fill_in('passenger[name]', with: 'Bar Foo')
+      click_button('Submit')
+      expect(page).to have_text('Passenger was successfully updated.')
+    end
+    scenario 'deleting an existing passenger' do 
+      passenger = create :passenger
+      visit passengers_path
+      expect(page).not_to have_link('Delete')
+    end
   end
 end
