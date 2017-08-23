c706031a014f528a5b68169da638ff85250eef51
AkivaGreen <green.akiva@outlook.com>
2017-08-23 16:12:48 -0400

    Fixed syntax of Timecop line

--- spec/models/log_entry_spec.rb
+++ spec/models/log_entry_spec.rb
@@ -6,7 +6,7 @@ describe LogEntry do
   describe 'entry_time' do
     it 'returns a string of created_at date and time' do
       time = '10:30 am, Thursday, March 16, 2017'
-      Timecop.freeze(Time.local(2017, 0o3, 16, 10, 30, 0)) do 
+      Timecop.freeze(Time.local(2017, 3, 16, 10, 30)) do 
         log_entry = create :log_entry
         expect(log_entry.entry_time).to eql time
       end
