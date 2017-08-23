13b1e2830b13f9dadc9e55be8a215b2fb57c1c6c
David Faulkenberry <david.faulkenberry@gmail.com>
2017-08-23 10:57:35 -0400

    Add staff status, but allow status to be blank

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -5,8 +5,8 @@ class Passenger < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-  STATUSES = %w[Alumni Faculty Student]
-  validates :status, presence: true, inclusion: { in: STATUSES }
+  STATUSES = %w[Alumni Faculty Staff Student]
+  validates :status, inclusion: { in: STATUSES, allow_blank: true }
 
   belongs_to :registerer, foreign_key: :registered_by, class_name: 'User',
     optional: true
