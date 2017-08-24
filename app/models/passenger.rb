d902e0502ccf02483347eb579131436d6742984d
David Faulkenberry <david.faulkenberry@gmail.com>
2017-08-24 14:23:42 -0400

    More compact method

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -22,12 +22,8 @@ class Passenger < ApplicationRecord
   belongs_to :mobility_device, optional: true
 
   def expiration_display
-    if permanent?
-      ''
-    elsif doctors_note.present? && doctors_note.expiration_date.present?
-      doctors_note.expiration_date.strftime '%m/%d/%Y'
-    else
-      'No Note'
+    unless permanent?
+      doctors_note.try(:expiration_date).try :strftime, '%m/%d/%Y' || 'No Note'
     end
   end
 
