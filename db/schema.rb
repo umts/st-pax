6aaa9b2df7e28d24f29b5bc5857c74f706734350
David Faulkenberry <david.faulkenberry@gmail.com>
2017-08-29 14:28:56 -0400

    Change column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829132804) do
+ActiveRecord::Schema.define(version: 20170829182335) do
 
   create_table "doctors_notes", force: :cascade do |t|
     t.integer "passenger_id"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20170829132804) do
   end
 
   create_table "mobility_devices", force: :cascade do |t|
-    t.string "device"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "needs_longer_rides", default: false, null: false
