3304408b0cc902b99675a1f65027a0b3a4239d52
Molly O'Neil <mboneil@umass.edu>
2017-09-01 10:40:57 -0400

    Replace 'overriden' with 'overridden' in db

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20170831135848) do
   create_table "doctors_notes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "passenger_id"
     t.date "expiration_date"
-    t.integer "overriden_by"
+    t.integer "overridden_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "override_until"
