a32bab2e29fc07ee71df7ced36184049e355b6f6
Chauce <ccchurch@umass.edu>
2017-09-08 13:01:10 -0400

    syntax fixes

--- app/assets/javascripts/temp_pax_options.js
+++ app/assets/javascripts/temp_pax_options.js
@@ -6,13 +6,17 @@ $(document).on('turbolinks:load', function(){
       registeredbyField.prop('disabled', true);
       registeredbyField.prop('checked', false);
     }
-    else registeredbyField.prop('disabled', false)
-         registeredbyField.prop('checked', false)
+    else {
+      registeredbyField.prop('disabled', false)
+      registeredbyField.prop('checked', false)
+    }
     if($(this).is(':checked')){
       hasbrochureField.prop('disabled', true);
       hasbrochureField.prop('checked', false);
     }
-    else hasbrochureField.prop('disabled', false);
-         hasbrochureField.prop('checked', false);
+    else {
+      hasbrochureField.prop('disabled', false);
+      hasbrochureField.prop('checked', false);
+    }
   });
 });
