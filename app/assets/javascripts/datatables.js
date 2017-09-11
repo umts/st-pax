3293da5cde0a425fb906fd7a02ae0f22ea0aa2ff
Chauce <ccchurch@umass.edu>
2017-09-11 12:55:22 -0400

    previous commit did not address issue

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -1,21 +1,3 @@
-if ( $.fn.dataTable.isDataTable( '#passengers.admin-table' ) ) {
-    table = $('#passengers.admin-table').DataTable();
-}
-else if {
-    table = $('#passengers.admin-table').DataTable( {
-        paging: false
-    } );
-}
-else if ( $.fn.dataTable.isDataTable( '#passengers.dispatch-table' ) ) {
-    table = $('#passengers.dispatch-table').DataTable();
-}
-else {
-    table = $('#passengers.dispatch-table').DataTable( {
-        paging: false
-    } );
-}
-
-
 $( document ).on("turbolinks:load", function() {
   $('#passengers.admin-table').DataTable({
     "aoColumns": [
