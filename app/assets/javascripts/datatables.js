c51fdfdf18704344130b1e74574df02eff8d130b
Chauce <ccchurch@umass.edu>
2017-09-12 13:24:00 -0400

    fixes logout error

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -1,32 +1,34 @@
 $( document ).on("turbolinks:load", function() {
-  $('#passengers.admin-table').DataTable({
-    "aoColumns": [
-      null,
-      null,
-      null,
-      { "bSortable": false },
-      { "bSortable": false },
-      null,
-      { "bSortable": false },
-      { "bSortable": false },
-      { "bSortable": false },
-    ],
-    "aLengthMenu": [[25, 50, 75, 100, -1], [25, 50, 75, 100, "All"]],
-    "iDisplayLength": 100
-  });
+  if ( !$.fn.dataTable.isDataTable( '#passengers' ) ) {
+    $('#passengers.admin-table').DataTable({
+      "aoColumns": [
+        null,
+        null,
+        null,
+        { "bSortable": false },
+        { "bSortable": false },
+        null,
+        { "bSortable": false },
+        { "bSortable": false },
+        { "bSortable": false },
+      ],
+      "aLengthMenu": [[25, 50, 75, 100, -1], [25, 50, 75, 100, "All"]],
+      "iDisplayLength": 100
+    });
 
-  $('#passengers.dispatch-table').DataTable({
-    "aoColumns": [
-      null,
-      null,
-      null,
-      { "bSortable": false },
-      { "bSortable": false },
-      null,
-      { "bSortable": false },
-      { "bSortable": false },
-    ],
-    "aLengthMenu": [[25, 50, 75, 100, -1], [25, 50, 75, 100, "All"]],
-    "iDisplayLength": 100
-  });
+    $('#passengers.dispatch-table').DataTable({
+      "aoColumns": [
+        null,
+        null,
+        null,
+        { "bSortable": false },
+        { "bSortable": false },
+        null,
+        { "bSortable": false },
+        { "bSortable": false },
+      ],
+      "aLengthMenu": [[25, 50, 75, 100, -1], [25, 50, 75, 100, "All"]],
+      "iDisplayLength": 100
+    });
+  }
 });
