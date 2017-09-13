fae8d5806b848e77409c720cbdbdc4b6a341393f
Chauce <ccchurch@umass.edu>
2017-09-13 13:41:16 -0400

    improvements on conditional

--- app/assets/javascripts/temp_pax_options.js
+++ app/assets/javascripts/temp_pax_options.js
@@ -2,21 +2,11 @@ $(document).on('turbolinks:load', function(){
   $('#passenger_permanent').change(function(){
     var registeredbyField = $('#pax_registered_with_DS');
     var hasbrochureField = $('#pax_has_brochure');
-    if($(this).is(':checked')){
-      registeredbyField.prop('disabled', true);
-      registeredbyField.prop('checked', false);
-    }
-    else {
-      registeredbyField.prop('disabled', false);
-      registeredbyField.prop('checked', false);
-    }
-    if($(this).is(':checked')){
-      hasbrochureField.prop('disabled', true);
-      hasbrochureField.prop('checked', false);
-    }
-    else {
-      hasbrochureField.prop('disabled', false);
-      hasbrochureField.prop('checked', false);
-    }
+
+    registeredbyField.prop('disabled', $(this).is(':checked'));
+    registeredbyField.prop('checked', false);
+
+    hasbrochureField.prop('disabled', $(this).is(':checked'));
+    hasbrochureField.prop('checked', false);
   });
 });
