43ade2c097aed3dff790b88a6b497c7bc17c2d88
Chauce <ccchurch@umass.edu>
2017-12-05 15:07:03 -0500

    javascript looks cleaner now

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -1,32 +1,17 @@
-$( document ).on("turbolinks:load", function() {
-  if ( !$.fn.dataTable.isDataTable( '#passengers' ) ) {
-    $('#passengers.admin-table').DataTable({
-      "aoColumns": [
-        null,
-        null,
-        null,
-        { "bSortable": false },
-        { "bSortable": false },
-        null,
-        { "bSortable": false },
-        { "bSortable": false },
-        { "bSortable": false },
-      ],
-      "aLengthMenu": [[25, 50, 75, 100, -1], [25, 50, 75, 100, "All"]],
-      "iDisplayLength": 100
-    });
-
-    $('#passengers.dispatch-table').DataTable({
-      "aoColumns": [
-        null,
-        null,
-        null,
-        { "bSortable": false },
-        { "bSortable": false },
-        null,
-        { "bSortable": false },
-        { "bSortable": false },
-      ],
+$(document).on("turbolinks:load", function() {
+  if (!$.fn.dataTable.isDataTable('#passengers')) {
+    var columns = [null,
+                   null,
+                   null,
+                   { "bSortable": false },
+                   { "bSortable": false },
+                   null,
+                   { "bSortable": false },
+                   { "bSortable": false }];
+    if($(this).hasClass('.admin-table')
+      columns.push({ "bSortable": false });
+    $('#passengers').DataTable({
+      "aoColumns": columns,
       "aLengthMenu": [[25, 50, 75, 100, -1], [25, 50, 75, 100, "All"]],
       "iDisplayLength": 100
     });
