17b674586cc38627459d8f5e804e8f043db9755f
Matt Moretti <werebus@gmail.com>
2019-01-10 15:27:22 -0500

    Let's use the auto-ids
    Now that `form_with` is auto-IDed, let's use that id

--- app/assets/javascripts/temp_pax_options.js
+++ app/assets/javascripts/temp_pax_options.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function(){
   $('#passenger_permanent').change(function(){
-    var registeredbyField = $('#pax_registered_with_DS');
-    var hasbrochureField = $('#pax_has_brochure');
+    var registeredbyField = $('#passenger_registered_with_disability_services');
+    var hasbrochureField = $('#passenger_has_brochure');
 
     registeredbyField.prop('disabled', $(this).is(':checked'));
     registeredbyField.prop('checked', false);
