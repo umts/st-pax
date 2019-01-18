a1e1b21f9ef5c20849aa4c588856a6b701bd1550
Matt Moretti <werebus@gmail.com>
2019-01-18 14:21:03 -0500

    Actually, lets make the nils false for this one too

--- db/migrate/20181204180756_change_active_and_permanent_to_not_null.rb
+++ db/migrate/20181204180756_change_active_and_permanent_to_not_null.rb
@@ -1,6 +1,6 @@
 class ChangeActiveAndPermanentToNotNull < ActiveRecord::Migration[5.1]
   def change
-    change_column_null :passengers, :active, false, true
+    change_column_null :passengers, :active, false, false
     change_column_null :passengers, :permanent, false, false
     change_column_default :passengers, :permanent, from: nil, to: false
   end
