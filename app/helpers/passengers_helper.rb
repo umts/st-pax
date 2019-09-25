330159ec9634303e2899dc25975bcbed32fae1e2
Anbranin <keichelman@gmail.com>
2019-09-25 19:46:03 -0400

    sort by date correctly

--- app/helpers/passengers_helper.rb
+++ app/helpers/passengers_helper.rb
@@ -19,11 +19,9 @@ module PassengersHelper
     doctors_note_row_class(passenger.doctors_note)
   end
 
-  def passengers_table_class
-    if @current_user.admin?
-      'row-border admin-table'
-    else
-      'row-border dispatch-table'
+  def sortable_date(note)
+    if note.present?
+      note.expiration_date.strftime('%Y%m%d%H%M%s')
     end
   end
 end
