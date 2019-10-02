b2b0fd6e9ff520055e153c2246188ec160881b6d
Anbranin <keichelman@gmail.com>
2019-10-02 12:12:24 -0400

    let migrations work with invalid data

--- db/migrate/20190924165620_add_active_status_data_to_passengers.rb
+++ db/migrate/20190924165620_add_active_status_data_to_passengers.rb
@@ -2,17 +2,19 @@ class AddActiveStatusDataToPassengers < ActiveRecord::Migration[5.2]
   def up
     Passenger.all.each do |passenger|
       if passenger.read_attribute(:active) == true
-        passenger.active!
+        passenger.active_status = 'active'
       else
-        passenger.archived!
+        passenger.active_status = 'archived'
       end
+      # there are invalid (no email) passengers in the database.
+      passenger.save(validate: false)
     end
   end
 
   def down
     Passenger.all.each do |passenger|
-      passenger.active = true
-      passenger.save
+      passenger.active = passenger.active?
+      passenger.save(validate: false)
     end
   end
 end
