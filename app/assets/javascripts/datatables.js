39703da436c4b38a488d50c4c263df39e345dcbd
Anbranin <keichelman@gmail.com>
2019-10-02 12:26:09 -0400

    performance boost offered by un-datatabling before caching

--- app/assets/javascripts/datatables.js
+++ app/assets/javascripts/datatables.js
@@ -1,7 +1,10 @@
 $( document ).on("turbolinks:load", function() {
   $('#passengers').DataTable({
     stateSave: true,
-    destroy: true,
     lengthMenu: [ [10, 25, 50, -1], [10, 25, 50, 'All'] ]
   });
+});
+
+$( document ).on('turbolinks:before-cache', function() {
+  $('#passengers').DataTable().destroy();
 });
