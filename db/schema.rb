292bdc04a7abeb53bfd6878866768837d27323fa
Anbranin <keichelman@gmail.com>
2019-10-07 15:02:19 -0400

    merge conflict resolution in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_144937) do
+ActiveRecord::Schema.define(version: 2019_10_07_185408) do
 
   create_table "doctors_notes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "passenger_id"
