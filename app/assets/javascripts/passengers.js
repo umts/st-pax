fab919e9506e63646fd37127fceb900a716f08d7
Anbranin <keichelman@gmail.com>
2019-10-08 13:16:15 -0400

    simplify javascript and latch onto .change event to prevent happening on edit page.

--- app/assets/javascripts/passengers.js
+++ app/assets/javascripts/passengers.js
@@ -1,13 +1,12 @@
 $( document ).on("turbolinks:load", function() {
-  $('#passenger_spire').focusout(function(){
+  $('#passenger_spire').change(function(){
     $.ajax({
       type: 'GET',
       url: '/passengers/check_existing',
       data: { spire_id: $(this).val() },
       success: function(response) {
         if(response == undefined){ return }
-        $('body').append('<div id="check-existing" class="modal" role="dialog" tabindex= "-1">')
-        $('#check-existing').html(response)
+        $('body').append(response)
         $('#check-existing').modal()
       }
     });
