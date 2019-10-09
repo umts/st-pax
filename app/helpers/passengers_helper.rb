88c2b1e9081c162408107f14e6320a993e76bd95
Chauce <ccchurch@umass.edu>
2019-10-09 10:30:48 -0400

    well there aren't any errors

--- app/helpers/passengers_helper.rb
+++ app/helpers/passengers_helper.rb
@@ -11,12 +11,22 @@ module PassengersHelper
     end
   end
 
+  def mobility_device(device)
+    if device.try(:needs_longer_rides) == true
+      'longer_ride'
+    end
+  end
+
   def passengers_table_row_class(passenger)
+    mobility_device(passenger.mobility_device)
+
     return if passenger.permanent?
     return 'no_note' if passenger.doctors_note.blank?
 
     doctors_note_row_class(passenger.doctors_note)
   end
+
+
 
   def sortable_date(note)
     if note.present?
