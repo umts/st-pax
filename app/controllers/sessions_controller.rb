16a3fa00e9d5821a2462a304a9cf6da03b0c73df
Anbranin <keichelman@gmail.com>
2019-10-18 15:38:23 -0400

    try to make it so you can log in as a passenger during development

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,15 +18,31 @@ class SessionsController < ApplicationController
       @admins = User.admins.order :name
       @dispatchers = User.dispatchers.order :name
     elsif request.post?
-      find_user
-      redirect_to passengers_path
+      if find_user
+        redirect_to passengers_path and return
+      end
+      set_passenger
+      redirect_to register_passengers_path and return
     end
   end
 
   private
 
   def find_user
-    user = User.find_by(id: params.require(:user_id))
-    session[:user_id] = user.id
+    user = User.find_by(id: params[:user_id])
+    session[:user_id] = user.id if user.present?
+  end
+
+  def set_passenger
+    passenger = Passenger.find_by(id: params[:passenger_id])
+    session[:passenger_id] = passenger.id if passenger.present?
+  end
+
+  def new_spire
+    if Passenger.any?
+      (Passenger.pluck(:spire).map(&:to_i).max + 1).to_s.rjust(8, '0') + '@umass.edu'
+    else
+      '00000000@umass.edu'
+    end
   end
 end
