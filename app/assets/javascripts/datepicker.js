22085c020faa4501bbad88863153a835c3992809
Anbranin <keichelman@gmail.com>
2019-10-18 15:35:51 -0400

    set a limit on the datepicker for more than 6 months.

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -2,4 +2,13 @@ $( document ).on("turbolinks:load", function() {
   $('.datepicker').datepicker({
     dateFormat: 'yy-mm-dd'
   });
+
+  $('.registration-datepicker').datepicker({
+    dateFormat: 'm/d/yy',
+    maxDate: monthLimit()
+  });
+
+  function monthLimit(){
+    return moment().add(6, 'months').calendar()
+  }
 });
