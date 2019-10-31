6f3343fdcb3efa373435e0a4329e855c046b8331
Anbranin <keichelman@gmail.com>
2019-10-31 13:40:29 -0400

    move all JS logic about passengers to passengers.js where I know where it is

--- app/assets/javascripts/passengers.js
+++ app/assets/javascripts/passengers.js
@@ -3,6 +3,14 @@ $( document ).on("turbolinks:load", function() {
     $('.doctors-note-fields').toggle(!$(this).prop('checked'));
   });
 
+  $('#passenger_permanent').change(function(){
+    var registeredbyField = $('#passenger_registered_with_disability_services');
+
+    registeredbyField.prop('disabled', $(this).is(':checked'));
+    registeredbyField.prop('checked', false);
+
+  });
+
   $('#passenger_spire').change(function(){
     $.ajax({
       type: 'GET',
--- app/assets/javascripts/temp_pax_options.js
+++
@@ -1,9 +0,0 @@
-$(document).on('turbolinks:load', function(){
-  $('#passenger_permanent').change(function(){
-    var registeredbyField = $('#passenger_registered_with_disability_services');
-
-    registeredbyField.prop('disabled', $(this).is(':checked'));
-    registeredbyField.prop('checked', false);
-
-  });
-});
