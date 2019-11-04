d6db62d4ce931c5a64665e7de492e1f7f2a071ea
Matt Moretti <werebus@gmail.com>
2019-11-04 13:01:44 -0500

    Nav bar fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,9 @@
 
 module ApplicationHelper
   def navbar_link(text, url)
-    button_classes = %w[button btn btn-default navbar-btn navbar-left]
-    button_classes << 'current' if current_page? url
-    link_to url do
-      content_tag :button, text, class: button_classes
-    end
+    button_classes = %w[mx-2 nav-link btn btn-outline-secondary]
+    button_classes << 'active' if current_page? url
+    link_to text, url, class: button_classes
   end
 
   def checkmark_glyph(value, yes: 'fa-check', no: 'fa-times')
