a339d2ff5ffca4c1df3a08bbc7542653fa6ba40f
Matt Moretti <werebus@gmail.com>
2019-11-06 08:19:03 -0500

    Also cover the case where we're given garbage

--- config/initializers/bootstrap-form-validation.rb
+++ config/initializers/bootstrap-form-validation.rb
@@ -2,7 +2,7 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   tag = Nokogiri::HTML::DocumentFragment.parse(html_tag)
     .css('label, textarea, input, select').first
 
-  case tag.node_name
+  case tag&.node_name
   when 'label'
     tag.to_s.html_safe
   when 'textarea', 'input', 'select'
