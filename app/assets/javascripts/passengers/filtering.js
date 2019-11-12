333c6ad64a3bd238b1ec677103941e50f8929147
Matt Moretti <werebus@gmail.com>
2019-11-12 14:27:15 -0500

    I decided to keep the filter name in the h1
    And, in the process, realized I wasn't fully dealing with the
    possiblility that not all pages have the filters on them.

--- app/assets/javascripts/passengers/filtering.js
+++ app/assets/javascripts/passengers/filtering.js
@@ -1,7 +1,12 @@
 function updatePassengerFilter() {
-  var selected = $("input[name=filter]:checked").val();
+  if (!$(".passenger-filters input").length) return;
+
+  var selected = $(".passenger-filters input[name=filter]:checked").val();
   var column = $("table#passengers th").index($(".permanent"));
 
+  $("span.filter-name").text(
+    selected.charAt(0).toUpperCase() + selected.slice(1)
+  );
   if (selected == 'all') selected = '';
   $("table#passengers").DataTable().columns(column).search(selected).draw();
 }
