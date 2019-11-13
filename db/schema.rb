cc4aba65c354f9ab343158d3225c726bae1ba36f
Anbranin <keichelman@gmail.com>
2019-11-13 12:05:36 -0500

    re-ran migration due to mistake

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_180258) do
+ActiveRecord::Schema.define(version: 2019_11_13_142931) do
 
   create_table "log_entries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
