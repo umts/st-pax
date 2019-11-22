0907de6c95ecbb675cfd267877f9050bc3010fd8
Anbranin <keichelman@gmail.com>
2019-11-22 16:51:57 -0700

    show verifying agency where applicable

--- app/helpers/passengers_helper.rb
+++ app/helpers/passengers_helper.rb
@@ -14,4 +14,15 @@ module PassengersHelper
       'expired'
     end
   end
+
+  def verification_information(passenger)
+    agency = passenger.eligibility_verification&.verifying_agency
+    if agency&.needs_contact_info?
+      render partial: 'contact_information',
+        locals: { verification: passenger.eligibility_verification }
+    else
+      agency&.name ||
+        'No agency has verified that this passenger requires rides.'
+    end
+  end
 end
