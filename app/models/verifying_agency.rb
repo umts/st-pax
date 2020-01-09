01d7809873b78cb85f26a2b45c3f03477dbadbfe
Anbranin <keichelman@gmail.com>
2020-01-09 12:11:56 -0500

    restrict deletion if there are any associated objects

--- app/models/verifying_agency.rb
+++ app/models/verifying_agency.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class VerifyingAgency < ApplicationRecord
-  has_many :eligibility_verifications
+  has_many :eligibility_verifications, dependent: :restrict_with_error
 
   validates :name, presence: true, uniqueness: true
 end
