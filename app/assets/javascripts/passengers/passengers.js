35e3fafd59ac31f483e87ca111dd4b32904d3e7d
tanvish <tanvishjha@gmail.com>
2020-01-10 16:39:05 -0500

    I accidentally pulled from master but then saved the previous changes before so I kinda messed up but I think we should be good now.

--- app/assets/javascripts/passengers/passengers.js
+++ app/assets/javascripts/passengers/passengers.js
@@ -1,12 +1,4 @@
 $( document ).on("turbolinks:load", function() {
-  $('#passenger_has_doctors_note').on('change', function(){
-    $('#passenger_doctors_note_attributes_expiration_date').prop('required', $(this).prop('checked'));
-  });
-
-  $('#passenger_permanent').on('change', function(){
-    $('.doctors-note-fields').toggle(!$(this).prop('checked'));
-  });
-
   var clipboard = new ClipboardJS('#copybtn', {
     text: function() {
       return $("#passengers tbody tr").map(function(){
