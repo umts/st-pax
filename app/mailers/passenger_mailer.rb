c28bb0d9b8e6f724ec7a05d4fd104116370a9489
Benjamin Melz <ben.j.melz@gmail.com>
2020-05-19 14:29:39 -0400

    Updated email template/subject

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,6 +1,6 @@
 class PassengerMailer < ApplicationMailer
   def notify_archived(passenger)
     @passenger = passenger
-    mail to: @passenger.email, subject: 'ST Account Archived'
+    mail to: @passenger.email, subject: 'Special Transit Account Archived'
   end
 end
