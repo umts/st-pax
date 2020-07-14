a2df32788040a3ab72b63ba6b83aff9a96caaa23
Anbranin <keichelman@gmail.com>
2020-07-14 10:29:54 -0400

    allow editing, telling them under which conditions they may

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :mobility_devices, except: :show
   resources :passengers do
     collection do
+      get :new_or_edit_registration
       get :brochure
       get :archived
       get :check_existing
