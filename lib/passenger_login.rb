365f9946d7c7b4f67f0354cc5e04ee836362eedd
Anbranin <keichelman@gmail.com>
2020-07-14 10:30:09 -0400

    IRL mail would be in the environment

--- lib/passenger_login.rb
+++ lib/passenger_login.rb
@@ -7,6 +7,7 @@ class PassengerLogin
     env['fcIdNumber'] = env['rack.session']['spire']
     env['givenName'] = env['rack.session']['first_name']
     env['surName'] = env['rack.session']['last_name']
+    env['mail'] = env['rack.session']['email']
     @app.call(env)
   end
 end
