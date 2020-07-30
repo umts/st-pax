3556004617d043ec5620ce5d0baa1172267da1f9
Joseph O'Leary <jpoleary@umass.edu>
2020-07-30 14:25:45 -0400

    should fix issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_183705) do
+ActiveRecord::Schema.define(version: 2020_07_29_191001) do
 
   create_table "eligibility_verifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "passenger_id"
