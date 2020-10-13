50e57e38e4d8f70aea24f2ba090e7b0858e21b29
Justin Forgue <juforgue@gmail.com>
2020-10-13 19:02:19 -0400

    test carrier update and destroy errors

--- spec/system/carriers_management_spec.rb
+++ spec/system/carriers_management_spec.rb
@@ -25,18 +25,20 @@ RSpec.describe 'Carrier Management', js: true do
           expect(page).to have_text '@vzw.com'
         end
       end
-      it 'verifies the presence of a name and gateway address' do
-        submit
-        expect(page).to have_text "Name can't be blank"
-        expect(page).to have_text "Gateway address can't be blank"
-      end
-      it 'verifies the uniqueness of name and gateway address' do
-        create :carrier, name: 'ATT', gateway_address: '@ATT.com'
-        fill_in 'Name', with: 'att'
-        fill_in 'Gateway address', with: '@att.com'
-        submit
-        expect(page).to have_text 'Name has already been taken'
-        expect(page).to have_text 'Gateway address has already been taken'
+      context 'fails to create the carrier' do
+        it 'verifies the presence of a name and gateway address' do
+          submit
+          expect(page).to have_text "Name can't be blank"
+          expect(page).to have_text "Gateway address can't be blank"
+        end
+        it 'verifies the uniqueness of name and gateway address' do
+          create :carrier, name: 'ATT', gateway_address: '@ATT.com'
+          fill_in 'Name', with: 'att'
+          fill_in 'Gateway address', with: '@att.com'
+          submit
+          expect(page).to have_text 'Name has already been taken'
+          expect(page).to have_text 'Gateway address has already been taken'
+        end
       end
       it 'goes back' do
         click_on 'Back'
@@ -65,11 +67,16 @@ RSpec.describe 'Carrier Management', js: true do
           expect(page).not_to have_text '@tmo.com'
         end
       end
+      it 'fails to update the carrier' do
+        fill_in 'Name', with: ''
+        submit
+        expect(page).to have_text "Name can't be blank"
+      end
     end
 
     context 'deleting a carrier' do
       before :each do
-        create :carrier, name: 'tmobile', gateway_address: '@tmo.com'
+        @carrier = create :carrier, name: 'tmobile', gateway_address: '@tmo.com'
         visit carriers_path
       end
       it 'successfully deletes the carrier' do
@@ -78,8 +85,17 @@ RSpec.describe 'Carrier Management', js: true do
         end
         expect(page).to have_text 'Carrier successfully destroyed'
         within 'tbody' do
-          expect(page).not_to have_text 'tmobile'
-          expect(page).not_to have_text '@tmo.com'
+          expect(page).to have_text 'No carriers to show.'
+        end
+      end
+      it 'fails to delete the carrier' do
+        allow_any_instance_of(Carrier).to receive(:destroy).and_return(false)
+        page.accept_confirm 'Are you sure?' do
+          click_button 'Destroy'
+        end
+        within 'tbody' do
+          expect(page).to have_text 'tmobile'
+          expect(page).to have_text '@tmo.com'
         end
       end
     end
