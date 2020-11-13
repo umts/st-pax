6dbf363515a0656691b3c3107dead7891cd7eaa3
Justin Forgue <juforgue@gmail.com>
2020-11-13 16:37:41 -0500

    validate carrier_gateway_address and add placeholders

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 class Carrier < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :gateway_address, presence: true,
                               uniqueness: { case_sensitive: false }
+  validates_format_of :gateway_address,
+                      with: /\A@([^@\s]+\.)+[^@\s]+\z/,
+                      message: 'must begin with @ and be followed by a name and domain'
   has_many :passengers
 end
