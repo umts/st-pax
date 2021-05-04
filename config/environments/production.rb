75dd3b1dcdec42180f5fa8ffbd2cdfbdc4fdc63b
Matt Moretti <werebus@gmail.com>
2021-05-04 14:18:34 -0400

    Flip on the remainder of the Rails 6.1 defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  config.action_dispatch.cookies_same_site_protection = :strict
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
