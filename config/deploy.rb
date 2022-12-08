70dbdb95b52a3c2cc92d905de62376ffe3cc2892
Matt Moretti <werebus@gmail.com>
2022-12-08 11:59:33 -0500

    Change 'master' to 'main'

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '~> 3.14'
 
 set :application, 'st-pax'
 set :repo_url, 'https://github.com/umts/st-pax.git'
-set :branch, :master
+set :branch, :main
 set :deploy_to, "/srv/#{fetch :application}"
 
 set :log_level, :info
