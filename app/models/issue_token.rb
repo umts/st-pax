bb45bc4094e609542651bc675a80e9aca4288e68
Matt Moretti <werebus@gmail.com>
2023-01-31 09:48:12 -0500

    Add validation of token functionality

--- app/models/issue_token.rb
+++ app/models/issue_token.rb
@@ -1,5 +1,6 @@
 class IssueToken < ApplicationRecord
-  validates :singleton, inclusion: { in: [0] }
+  validates :singleton, inclusion: { in: [0] }, uniqueness: true
+  validate :functioning_token
 
   class << self
     delegate :token, to: :instance
@@ -7,5 +8,17 @@ class IssueToken < ApplicationRecord
     def instance
       create_with(singleton: 0).first_or_create
     end
+  end
+
+  private
+
+  def functioning_token
+    return if token.blank?
+
+    client = Octokit::Client.new client_id: Rails.application.credentials.dig(:github, :client_id),
+                                 client_secret: Rails.application.credentials.dig(:github, :client_secret)
+    client.check_application_authorization token
+  rescue Octokit::NotFound
+    errors.add :token, 'is not valid or is not authorized'
   end
 end
