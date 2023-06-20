3d9e130a03ab43025b07470ce24e6f891609a4ad
Andrew Flammia <aflam745@gmail.com>
2023-06-20 15:26:34 -0400

    abstracted click function

--- app/assets/javascripts/log_entries.js
+++ app/assets/javascripts/log_entries.js
@@ -1,33 +1,27 @@
 $(document).on('turbolinks:load', function() {
+  function setLogEntryEditing(logEntry, change) {
+    var edit = logEntry.find('.log-entry-edit-button');
+    var destroy = logEntry.find('.log-entry-delete-button');
+    var show = logEntry.find('.log-entry-show');
+    var form = logEntry.find('.log-entry-edit');
+    var cancel = logEntry.find('.log-entry-edit-cancel-button');
+
+    edit.prop('hidden', change);
+    destroy.prop('hidden', change);
+    show.prop('hidden', change);
+    form.prop('hidden', !(change));
+    cancel.prop('hidden', !(change));
+  }
+
   $('.log-entry-edit-button').on('click', function (e) {
     var theClickedElement = $(e.target);
     var parent = theClickedElement.closest('.log-entry');
-    var edit = parent.find('.log-entry-edit-button');
-    var destroy = parent.find('.log-entry-delete-button');
-    var show = parent.find('.log-entry-show');
-    var form = parent.find('.log-entry-edit');
-    var cancel = parent.find('.log-entry-edit-cancel-button');
-
-    edit.prop('hidden', true);
-    destroy.prop('hidden', true);
-    show.prop('hidden', true);
-    form.prop('hidden', false);
-    cancel.prop('hidden', false);
+    setLogEntryEditing(parent, true);
   });
 
   $('.log-entry-edit-cancel-button').on('click', function (e) {
     var theClickedElement = $(e.target);
     var parent = theClickedElement.closest('.log-entry');
-    var edit = parent.find('.log-entry-edit-button');
-    var destroy = parent.find('.log-entry-delete-button');
-    var show = parent.find('.log-entry-show');
-    var form = parent.find('.log-entry-edit');
-    var cancel = parent.find('.log-entry-edit-cancel-button');
-
-    edit.prop('hidden', false);
-    destroy.prop('hidden', false);
-    show.prop('hidden', false);
-    form.prop('hidden', true);
-    cancel.prop('hidden', true);
+    setLogEntryEditing(parent, false);
   });
 });
