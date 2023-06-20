1a51d89893ee55d3e925edb12b67489c22a41dd3
Andrew Flammia <aflam745@gmail.com>
2023-06-20 14:20:56 -0400

    Fixed linting issues

--- app/assets/javascripts/log_entries.js
+++ app/assets/javascripts/log_entries.js
@@ -1,16 +1,15 @@
 $(document).on('turbolinks:load', function() {
-  // andrew writes javascript here
   $('.log-entry-edit-button').on('click', function (e) {
     var theClickedElement = $(e.target);
     var parent = theClickedElement.closest('.log-entry');
     var edit = parent.find('.log-entry-edit-button');
-    var delet = parent.find('.log-entry-delete-button');
+    var destroy = parent.find('.log-entry-delete-button');
     var show = parent.find('.log-entry-show');
     var form = parent.find('.log-entry-edit');
     var cancel = parent.find('.log-entry-edit-cancel-button');
 
     edit.prop('hidden', true);
-    delet.prop('hidden', true);
+    destroy.prop('hidden', true);
     show.prop('hidden', true);
     form.prop('hidden', false);
     cancel.prop('hidden', false);
@@ -20,13 +19,13 @@ $(document).on('turbolinks:load', function() {
     var theClickedElement = $(e.target);
     var parent = theClickedElement.closest('.log-entry');
     var edit = parent.find('.log-entry-edit-button');
-    var delet = parent.find('.log-entry-delete-button');
+    var destroy = parent.find('.log-entry-delete-button');
     var show = parent.find('.log-entry-show');
     var form = parent.find('.log-entry-edit');
     var cancel = parent.find('.log-entry-edit-cancel-button');
 
     edit.prop('hidden', false);
-    delet.prop('hidden', false);
+    destroy.prop('hidden', false);
     show.prop('hidden', false);
     form.prop('hidden', true);
     cancel.prop('hidden', true);
