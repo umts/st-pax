14ad85c2d09c16584b87c0a59b147cfdc9756225
benmelz <ben.j.melz@gmail.com>
2023-06-21 10:06:23 -0400

    Restyle dispatch log (#366)
    Refreshes the styling for the log_entry system

--- app/assets/javascripts/log_entries.js
+++ app/assets/javascripts/log_entries.js
@@ -1,10 +1,27 @@
 $(document).on('turbolinks:load', function() {
-  $('.dispatch-log').on('click', '.edit-entry', function(e) {
-    e.preventDefault()
-    var li = $(this).parents('li');
-    li.find('.text p').hide();
-    li.find('.text .edit-form').show();
-    li.find('.actions .button_to').hide();
-    $(this).hide()
+  function setLogEntryEditing(logEntry, change) {
+    var edit = logEntry.find('.log-entry-edit-button');
+    var destroy = logEntry.find('.log-entry-delete-button');
+    var show = logEntry.find('.log-entry-show');
+    var form = logEntry.find('.log-entry-edit');
+    var cancel = logEntry.find('.log-entry-edit-cancel-button');
+
+    edit.prop('hidden', change);
+    destroy.prop('hidden', change);
+    show.prop('hidden', change);
+    form.prop('hidden', !(change));
+    cancel.prop('hidden', !(change));
+  }
+
+  $('.log-entry-edit-button').on('click', function (e) {
+    var theClickedElement = $(e.target);
+    var parent = theClickedElement.closest('.log-entry');
+    setLogEntryEditing(parent, true);
+  });
+
+  $('.log-entry-edit-cancel-button').on('click', function (e) {
+    var theClickedElement = $(e.target);
+    var parent = theClickedElement.closest('.log-entry');
+    setLogEntryEditing(parent, false);
   });
 });
