52ab224cd373c59d4dc8cf090f7fca67bebeb059
Matt Moretti <werebus@gmail.com>
2023-09-20 09:08:05 -0400

    PowerShell script for looking up Spire IDs
    It takes a CSV file as an argument, expects that CSV to contain
    (internal) IDs and Spire IDs, it creates a new CSV that contains IDs,
    NetIDs and UIDs.

---
+++ contrib/spire-lookup.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = "Stop"
+
+$inFile = Get-ChildItem $args[0]
+$outFile = New-Item -ItemType File -Path "$($inFile.BaseName)-result$($inFile.Extension)"
+
+foreach($row in (Import-Csv -Path $inFile)) {
+  $userLookup = Get-ADUser -Filter "employeeID -eq $($row.spire_id) -and employeeType -like 'P*'" `
+                           -Properties UserPrincipalName,msDS-ExternalDirectoryObjectId
+
+  If ($userLookup -ne $null) {
+    # External object IDs are prefixed with the object type
+    # e.g. 'User_0d0d947f-25b0-4a5a-b9a8-0094d1221ca3'
+    $uid = $userLookup.("msDS-ExternalDirectoryObjectId").split("_")[-1]
+
+    New-Object -TypeName PSObject -Property @{
+      id = $row.id
+      net_id = $userLookup.UserPrincipalName
+      uid = $uid
+    } | Export-Csv -Path $outFile -Append
+  }
+}
