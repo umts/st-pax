8a2a4f2e5819a0fa77865792fafa3249e789bc8e
Matt Moretti <werebus@gmail.com>
2024-01-12 15:16:57 -0500

    Ran app:update task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_26_202921) do
+ActiveRecord::Schema[6.1].define(version: 2024_01_26_202921) do
 
   create_table "eligibility_verifications", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.integer "passenger_id"
