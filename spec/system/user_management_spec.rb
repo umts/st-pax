d4a386dc6f1309cf11422385e69f90c788f3be06
Matt Moretti <werebus@gmail.com>
2024-01-17 23:38:02 -0500

    Rubocopped user system spec

--- spec/system/user_management_spec.rb
+++ spec/system/user_management_spec.rb
@@ -3,77 +3,70 @@
 require 'rails_helper'
 
 RSpec.describe 'User Management' do
+  let!(:user) { create(:user) }
+
   before do
-    @user = create(:user)
     when_current_user_is :admin
+    visit users_path
   end
 
-  context 'deleting a user unsuccessfully', :js do
+  context 'when deleting a user', :js do
+    subject :submit do
+      page.accept_confirm 'Are you sure?' do
+        within('tr', text: user.name) { click_on 'Destroy' }
+      end
+    end
+
+    context 'when a user is not deletable' do
+      before { create(:log_entry, user:) }
+
+      it 'puts the errors in the flash' do
+        submit
+        expect(page).to have_text('Cannot delete record because dependent log entries exist')
+      end
+    end
+
+    context 'when a user is deletable' do
+      it 'deletes the user and says it did' do
+        submit
+        expect(page).to have_text('User successfully destroyed')
+      end
+    end
+  end
+
+  context 'when editing a user' do
     before do
-      @log = create(:log_entry, user: @user)
+      within('tr', text: user.name) { click_on 'Edit' }
     end
 
-    it 'puts the errors in the flash' do
-      visit users_path
-      # Click the 'destroy' link in the user's row
-      page.accept_confirm 'Are you sure?' do
-        within('tr', text: @user.name) { click_on 'Destroy' }
-      end
-      expect(page).to have_text(
-        'Cannot delete record because dependent log entries exist'
-      )
-    end
-  end
-
-  context 'deleting a user successfully', :js do
-    it 'deletes the user and says it did' do
-      visit users_path
-      page.accept_confirm 'Are you sure?' do
-        within('tr', text: @user.name) { click_on 'Destroy' }
-      end
-      expect(page).to have_text(
-        'User successfully destroyed'
-      )
-    end
-  end
-
-  context 'editing an existing user successfully' do
     it 'updates the user' do
-      visit users_path
-      within('tr', text: @user.name) { click_on 'Edit' }
       fill_in 'Name', with: 'Bar Foo'
-      click_button 'Save'
-      expect(page).to have_text 'User successfully updated'
+      click_on 'Save'
+      expect(page).to have_text('User successfully updated')
     end
-  end
 
-  context 'editing an existing user unsuccessfully' do
     it 'puts the error in the flash' do
-      visit users_path
-      within('tr', text: @user.name) { click_on 'Edit' }
       fill_in 'Spire', with: 'not a valid spire'
-      click_button 'Save'
-      expect(page).to have_text 'Spire must be 8 digits followed by @umass.edu'
+      click_on 'Save'
+      expect(page).to have_text('Spire must be 8 digits followed by @umass.edu')
     end
   end
 
-  context 'creating a new user successfully' do
+  context 'when creating a new user' do
+    before do
+      click_on 'New User'
+    end
+
     it 'creates the user' do
-      visit users_path
-      click_link 'New User'
       fill_in 'Name', with: 'Foo Bar'
       fill_in 'Spire', with: '12345678@umass.edu'
-      click_button 'Save'
+      click_on 'Save'
       expect(page).to have_text 'User successfully created'
     end
-  end
 
-  context 'creating a new user unsuccessfully' do
     it 'renders an error in the flash' do
-      visit users_path
-      click_link 'New User'
       fill_in 'Spire', with: 'invalid spire'
-      click_button 'Save'
+      click_on 'Save'
       expect(page).to have_text 'Spire must be 8 digits followed by @umass.edu'
     end
   end
