7bb88303946ea11c71daee8227e5155f9a278b03
Matt Moretti <werebus@gmail.com>
2024-01-26 16:49:20 -0500

    Added "Staff title" to User views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :spire, :active, :admin)
+    params.require(:user).permit(:name, :spire, :active, :admin, :title)
   end
 end
