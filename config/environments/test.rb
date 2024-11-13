66c4ea90bf3b60924da7cda11dd62d17923f252d
benmelz <ben@melz.me>
2024-11-13 10:33:21 -0500

    copy the version from umaps/round-three

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,9 +11,9 @@ Rails.application.configure do
 
   config.middleware.use PassengerLogin
 
-  # While tests run files are not watched, reloading is not necessary.
-  config.enable_reloading = false
-  config.cache_classes = false
+  # Turn on reloading for spring.
+  config.enable_reloading = true
+  config.action_view.cache_template_loading = true
 
   # Eager loading loads your entire application. When running a single test locally,
   # this is usually not necessary, and can slow down your test suite. However, it's
