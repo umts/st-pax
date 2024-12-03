10d720aee589ae90a322fab76e6c593eaf7397d1
Matt Moretti <werebus@gmail.com>
2024-12-03 12:19:15 -0500

    Upgrade Bootstrap to 5.x (#487)
    * Update Bootstrap npm package
    
    * Update bootstrap_form gem
    
    * Update to BS5 versions of datatables
    
    * Fix colors - can't only override $theme-colors anymore
    
    * Fixup Navbar
    
    * Fix existing pax modal
    
    * Various Bootstrap 5 style fixes
    
    * Fix table backgrounds
    * Fix close-button on flash
    * Tidy admin table pages
    * Fixup log entry form
    * User form, no need for check-box groups
    * Tidy passenger form
    * Fixed input groups in dev login
    
    * Fix required field errors
    
    It turns out that `bootstrap_form` was supposed to be making fields for
    required attributes `required` all along and it just... wasn't?
    
    Well, now it is. So, no, those errors aren't in the flash because we can't
    submit the form.
    
    * We can use bootstrap_form's checkboxes
    
    (Now that all checkboxes are "custom" checkboxes)
    
    * `navbar-light` is deprecated
    
    * Put :data back in CSP image policy

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 source 'https://rubygems.org'
 ruby file: '.ruby-version'
 
-gem 'bootstrap_form'
+gem 'bootstrap_form', '~> 5.4'
 gem 'business_time'
 gem 'cssbundling-rails'
 gem 'exception_notification'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,11 @@
 // about supported directives.
 //
 //= require jquery/dist/jquery
-//= require popper.js/dist/umd/popper
+//= require @popperjs/core/dist/umd/popper
 //= require bootstrap/dist/js/bootstrap
 //= require datatables.net/js/dataTables.js
 //= require datatables.net-fixedheader/js/dataTables.fixedHeader.js
-//= require datatables.net-bs4/js/dataTables.bootstrap4.js
+//= require datatables.net-bs5/js/dataTables.bootstrap5.js
 //= require rails-ujs
 //= require turbolinks
 //= require clipboard/dist/clipboard
