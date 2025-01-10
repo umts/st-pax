e03b4902aa04ad738127932c69afbece3a152ef1
Alex Locke <alocke_8181@yahoo.com>
2025-01-10 12:25:38 -0500

    GET /new specs

--- spec/requests/mobility_devices_spec.rb
+++ spec/requests/mobility_devices_spec.rb
@@ -37,4 +37,35 @@ RSpec.describe 'Mobility Devices' do
       end
     end
   end
+
+  describe 'GET /new' do
+    subject(:call) { get '/mobility_devices/new' }
+
+    context 'when logged in as admin' do
+      before { login_as admin }
+
+      it 'responds successfully' do
+        call
+        expect(response).to be_successful
+      end
+    end
+
+    context 'when logged in as a user' do
+      before { login_as user }
+
+      it 'responds with an unauthorized status' do
+        call
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+
+    context 'when logged in as a passenger' do
+      before { login_as_passenger pax }
+
+      it 'responds with an unauthorized status' do
+        call
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+  end
 end
