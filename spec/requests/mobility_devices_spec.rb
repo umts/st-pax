cc417ee105d88de68423dfe6d386f67c178a3a57
Alex Locke <alocke_8181@yahoo.com>
2025-01-10 13:19:52 -0500

    POST /mobility_devices/new specs

--- spec/requests/mobility_devices_spec.rb
+++ spec/requests/mobility_devices_spec.rb
@@ -7,8 +7,46 @@ RSpec.describe 'Mobility Devices' do
   let(:user) { create(:user) }
   let(:pax) { create(:passenger) }
 
-  describe 'GET /index' do
+  describe 'GET /mobility_devices' do
     subject(:call) { get '/mobility_devices' }
+
+    context 'when logged in as admin' do
+      before { login_as admin }
+
+      it 'responds successfully' do
+        call
+        expect(response).to be_successful
+      end
+    end
+
+    context 'when logged in as a user' do
+      before { login_as user }
+
+      it 'responds with an unauthorized status' do
+        call
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+
+    context 'when logged in as a passenger' do
+      before { login_as_passenger pax }
+
+      it 'responds with an unauthorized status' do
+        call
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+
+    context 'when not logged in' do
+      it 'responds with an unauthorized status' do
+        call
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+  end
+
+  describe 'GET /mobility_devices/new' do
+    subject(:call) { get '/mobility_devices/new' }
 
     context 'when logged in as admin' do
       before { login_as admin }
@@ -38,32 +76,93 @@ RSpec.describe 'Mobility Devices' do
     end
   end
 
-  describe 'GET /new' do
-    subject(:call) { get '/mobility_devices/new' }
+  describe 'POST /mobility_devices' do
+    subject(:submit) { post '/mobility_devices', params: }
+
+    let(:params) { { mobility_device: { name: 'Scooter', needs_longer_rides: true } } }
 
     context 'when logged in as admin' do
       before { login_as admin }
 
-      it 'responds successfully' do
-        call
-        expect(response).to be_successful
+      context 'with valid params' do
+        it 'creates a new mobility device' do
+          expect { submit }.to change(MobilityDevice, :count).by(1)
+        end
+
+        it 'creates a new mobility device with the right attributes' do
+          submit
+          expect(MobilityDevice.last).to have_attributes({ name: 'Scooter',
+                                                           needs_longer_rides: true })
+        end
+
+        it 'redirects to the index' do
+          submit
+          expect(response).to redirect_to mobility_devices_url
+        end
+      end
+
+      context 'when the device already exists' do
+        before { submit }
+
+        let(:copy_params) { { mobility_device: { name: 'sCoOtEr', needs_longer_rides: false } } }
+
+        it 'does not create a new device' do
+          expect { post '/mobility_devices', params: copy_params }.not_to change(MobilityDevice, :count)
+        end
+
+        it 'returns an unprocessable entity status' do
+          post '/mobility_devices', params: copy_params
+          expect(response).to have_http_status :unprocessable_entity
+        end
+      end
+
+      context 'with invalid params' do
+        let(:bad_params) { { mobility_device: { name: nil, needs_longer_rides: 'true' } } }
+
+        it 'does not create a new device' do
+          expect { post '/mobility_devices', params: bad_params }.not_to change(MobilityDevice, :count)
+        end
+
+        it 'returns an unprocessable entity status' do
+          post '/mobility_devices', params: bad_params
+          expect(response).to have_http_status :unprocessable_entity
+        end
       end
     end
 
     context 'when logged in as a user' do
       before { login_as user }
 
-      it 'responds with an unauthorized status' do
-        call
+      it 'does not create a new device' do
+        expect { submit }.not_to change(MobilityDevice, :count)
+      end
+
+      it 'returns an unauthorized status' do
+        submit
         expect(response).to have_http_status :unauthorized
       end
     end
 
-    context 'when logged in as a passenger' do
+    context 'when logged in as a pax' do
       before { login_as_passenger pax }
 
-      it 'responds with an unauthorized status' do
-        call
+      it 'does not create a new device' do
+        expect { submit }.not_to change(MobilityDevice, :count)
+      end
+
+      it 'returns an unauthorized status' do
+        submit
+        expect(response).to have_http_status :unauthorized
+      end
+    end
+
+    context 'when not logged in' do
+      it 'does not create a new device' do
+        expect { submit }.not_to change(MobilityDevice, :count)
+      end
+
+      it 'returns an unauthorized status' do
+        submit
         expect(response).to have_http_status :unauthorized
       end
     end
