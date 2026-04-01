ddf9d26b1561b44e1480fef5f292f0b3b7ee8dd0
benmelz <ben.j.melz@gmail.com>
2026-04-01 13:34:10 -0400

    Add dispatch log export (#914)

--- app/models/log_entry.rb
+++ app/models/log_entry.rb
@@ -8,4 +8,15 @@ class LogEntry < ApplicationRecord
   def entry_time
     created_at.strftime '%A, %B %e, %Y — %l:%M %P'
   end
+
+  def self.to_export_csv
+    CSV.generate headers: %w[text pinned spire created_at updated_at], write_headers: true do |csv|
+      find_each do |log_entry|
+        csv << {
+          'text' => log_entry.text, 'pinned' => log_entry.pinned, 'spire' => log_entry.user.spire.chomp('@umass.edu'),
+          'created_at' => log_entry.created_at.iso8601, 'updated_at' => log_entry.updated_at.iso8601
+        }
+      end
+    end
+  end
 end
