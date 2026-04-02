921d6edd0a5e652de9ddb328c6fe7ed14c2da06b
benmelz <ben.j.melz@gmail.com>
2026-04-02 14:18:51 -0400

    Disable dispatch log (#916)

--- app/controllers/log_entries_controller.rb
+++ app/controllers/log_entries_controller.rb
@@ -4,39 +4,18 @@ class LogEntriesController < ApplicationController
   before_action :find_modifiable_entry, only: %i[destroy update]
 
   def index
-    @entry = LogEntry.new
-    @entries = LogEntry.includes(:user).order(pinned: :desc, created_at: :desc)
-                       .page(params[:page]).per(100)
+    redirect_to 'https://umasstransit.org/dispatch_logs?redirected=true', allow_other_host: true
   end
 
   def create
-    @entry = LogEntry.new entry_params.merge(user: Current.user)
-    if @entry.save
-      flash[:success] = t('.success')
-    else
-      flash[:danger] = @entry.errors.full_messages
-    end
     redirect_to log_entries_path
   end
 
   def update
-    if @entry.update entry_params
-      flash[:success] = t('.success')
-    else
-      flash[:danger] = @entry.errors.full_messages
-    end
     redirect_to log_entries_path
   end
 
   def destroy
-    if @entry.destroy
-      flash[:success] = t('.success')
-    else
-      # skip coverage until LogEntry deletion can possibly be restrained
-      # :nocov:
-      flash[:danger] = @log.errors.full_messages
-      # :nocov:
-    end
     redirect_to log_entries_path
   end
 
